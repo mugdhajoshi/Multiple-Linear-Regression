400c3ad94531ab7e7916908413e4fceb17472dd8
mugdhajoshi <36741026+mugdhajoshi@users.noreply.github.com>
2018-07-14 22:13:47 -0700

    optimal model using Backward elimination

--- MultipleLinearRegression.py
+++ MultipleLinearRegression.py
@@ -32,3 +32,23 @@ regressor.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
+
+#Building optimal model using Backward elimination
+import statsmodels.formula.api as sm
+X=np.append(arr=np.ones((50,1)).astype(int),values=X,axis=1)
+X_opt=X[:, [0,1,2,3,4,5]]
+
+regressor_OLS=sm.OLS(endog=y,exog=X_opt).fit()
+regressor_OLS.summary()
+
+X_opt=X[:, [0,1,2,4,5]]
+regressor_OLS=sm.OLS(endog=y,exog=X_opt).fit()
+regressor_OLS.summary()
+
+X_opt=X[:, [0,1,4,5]]
+regressor_OLS=sm.OLS(endog=y,exog=X_opt).fit()
+regressor_OLS.summary()
+
+X_opt=X[:, [0,1,4]]
+regressor_OLS=sm.OLS(endog=y,exog=X_opt).fit()
+regressor_OLS.summary()
